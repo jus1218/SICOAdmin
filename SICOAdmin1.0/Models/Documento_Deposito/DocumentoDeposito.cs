0f198e5672e36cbe52870b65cecd63d6d95735b1
DanielChevez <cali1511a.com@gmail.com>
2023-04-19 11:51:37 -0600

    Agregando validaciones de fechas

---
+++ SICOAdmin1.0/Models/Documento_Deposito/DocumentoDeposito.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SICOAdmin1._0.Models.Documento_Deposito
+{
+    public class DocumentoDeposito
+    {
+       public string NombreTabla { get; set; }
+        public string NombreTablaAuxiliar { get; set; }
+        public string DocumentoDebito { get; set; }
+        public string DocumentoCredito { get; set; }
+        public System.DateTime FechaDocumento { get; set; }
+        public int CondicionPago { get; set; }
+        public string Nota { get; set; }
+        public int IdPartida { get; set; }
+
+        public decimal Monto { get; set; }
+
+
+    }
+}
